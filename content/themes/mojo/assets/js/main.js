3bddaf1f66343460cee322e97f98f083493e5e9a
Ryan Coughlin <ryan@ryancoughlin.com>
2014-02-06 11:10:05 -0500

    Remove code wrapping for pre tags

--- content/themes/mojo/assets/js/main.js
+++ content/themes/mojo/assets/js/main.js
@@ -1,15 +1,9 @@
 (function ($) {
-
   var $menuButton = $("#draw-menu");
-  var $codeBlocks = $("code");
-
   $(document).ready(function() {
 
     if (Modernizr.touch) {
       $menuButton.addClass("absolute");
     }
-
-    $codeBlocks.wrap( "<pre></pre>" );
-
   });
 }(jQuery));
